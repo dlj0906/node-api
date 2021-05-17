47cc15d4cd04042dff553f5891d6566369f6ec1c
donglongjiang <donglongjiang@xingbangdigital.com>
2021-05-17 16:46:02 +0800

    feat(结构): 修改成单引号

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,12 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: ['prettier', 'plugin:prettier/recommended'],
+  extends: ["prettier", "plugin:prettier/recommended"],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  rules: {},
+  rules: {
+    "no-unused-vars": "warn", //把该条提示信息转换成警告信息
+  },
 };
--- app.js
+++ app.js
@@ -4,23 +4,23 @@
  * @date 2021/5/12
  */
 
-import bodyParser from "body-parser";
+import bodyParser from 'body-parser'
 // 引入body-parser模块
-import express from "express";
+import express from 'express'
 // 引入express模块
-import cors from "cors";
+import cors from 'cors'
 // 引入cors模块
-import routes from "./routes/index"; //导入自定义路由文件，创建模块化路由
-const app = express();
+import routes from './routes/index' //导入自定义路由文件，创建模块化路由
+const app = express()
 
-app.use(bodyParser.json()); // 解析json数据格式
-app.use(bodyParser.urlencoded({ extended: true })); // 解析form表单提交的数据application/x-www-form-urlencoded
+app.use(bodyParser.json()) // 解析json数据格式
+app.use(bodyParser.urlencoded({ extended: true })) // 解析form表单提交的数据application/x-www-form-urlencoded
 
-app.use(cors()); // 注入cors模块解决跨域
+app.use(cors()) // 注入cors模块解决跨域
 
-app.use("/", routes);
+app.use('/', routes)
 
 app.listen(9000, () => {
   // 监听8088端口
-  console.log("服务已启动 http://localhost:9000");
-});
+  console.log('服务已启动 http://localhost:9000')
+})
